a0bbeb63e78b5c68451b6aec5c33c207695c6bf2
AleksyBalazinski <aleksy.j.balazinski@gmail.com>
2022-12-25 13:49:59 +0100

    add quiz variants

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -7,10 +7,71 @@ class QuizPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Quiz page'),
+        title: const Text('Quiz selection page'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('Voivodeship on the map'),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const VoivodeshipOnMapQuiz(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            title: const Text('Position of the voivodeship'),
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const PositionOfVoivodeship(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class VoivodeshipOnMapQuiz extends StatelessWidget {
+  const VoivodeshipOnMapQuiz({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Voivodeship on the map'),
+        leading: InkWell(
+          onTap: () => Navigator.pop(context),
+          child: const Icon(Icons.arrow_back_ios),
+        ),
       ),
       body: const Center(
-        child: Text('various quizes go here'),
+        child: Text('Voivodeship on the map quiz goes here'),
+      ),
+    );
+  }
+}
+
+class PositionOfVoivodeship extends StatelessWidget {
+  const PositionOfVoivodeship({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Position of the voivodeship'),
+        leading: InkWell(
+          onTap: () => Navigator.pop(context),
+          child: const Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      body: const Center(
+        child: Text('Position of the voivodeship quiz goes here'),
       ),
     );
   }
